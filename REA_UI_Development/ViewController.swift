5714c5013aba2ac0e19caf176f911e7d4dc03dda
Fan Wu <wf9877@wufans-mbp.gateway>
2018-07-14 10:19:28 +1000

    Initial Commit

---
+++ REA_UI_Development/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  REA_UI_Development
+//
+//  Created by Fan Wu on 7/14/18.
+//  Copyright © 2018 8184. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
